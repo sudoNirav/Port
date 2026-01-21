aa5845d1f8e5397da273f55a14914a6e568dffe5
sudoNirav <niravp1201@gmail.com>
2026-01-22 00:53:58 +0530

    now it's all next.js :)

---
+++ homelab-frontend-temp/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"m0zBBnGqkYV/Qi28h9w467DmX+fzaaFqrE8Arfrfdd0=\"\n}"
--- homelab-frontend-temp/src/config/api_urls.tsx
+++
@@ -1,1 +0,0 @@
-export const backendBaseURL = "http://localhost:3000";

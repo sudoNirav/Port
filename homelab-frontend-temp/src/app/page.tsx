2ea4a6ace01801182221d4b68da3e03d9a73fb7a
sudoNirav <niravp1201@gmail.com>
2026-01-28 16:12:27 +0530

    ignore

--- homelab-frontend-temp/src/app/page.tsx
+++ homelab-frontend-temp/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/dist/client/link";
 export default function HomePage() {
   return (
     <div>
-      <h1>Hi, this is the lander (for now) curl</h1>
+      <h1>Hi, this is the lander (for now).</h1>
       <button>
         <Link href="/login">Go to login</Link>
       </button>

e63bea9684367a9b6c5d724c75e1581f77d1e8ad
sudoNirav <niravp1201@gmail.com>
2026-01-28 15:53:02 +0530

    test 2

--- homelab-frontend-temp/src/app/page.tsx
+++ homelab-frontend-temp/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/dist/client/link";
 export default function HomePage() {
   return (
     <div>
-      <h1>Hi, this is the lander (for now) . TESTSSSS</h1>
+      <h1>Hi, this is the lander (for now) . TESTSSSS 2</h1>
       <button>
         <Link href="/login">Go to login</Link>
       </button>
